50c7e476dbeff8f793254810b5e628c63a4a3c0d
espoir <hpmasevo@gmail.com>
2022-12-27 16:30:30 +0100

    test pour afficher les images provenant de l'API

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 
-/*
-function createimage(tableau) {
-  for (item of tableau) {
-    let simg = document.createElement('img');
-    simg.setAttribute('src', item.url);
-    document.body.appendChild(simg);
+if (window.HTMLTitleElement = "page") 
+{
+  function createimage(tableau) {
+    for (item of tableau) {
+      let simg = document.createElement('img');
+      simg.setAttribute('src', item.url);
+      document.body.appendChild(simg);
+    }
   }
 }
 
-fetch("data.json");
-   .then((response) => response.json())
-  .then((json) => createimage(json))*/
-
-  const button = document.getElementById("animal");
-  let image = document.getElementById("image");
 
   function photos()
   {
@@ -25,22 +21,9 @@ fetch("data.json");
   };
 
 photos();
+button.addEventListener("click", photos);
 
 
 
-for (let i = 0; i < 50; i++) {
 
-  function demarrage() 
-  {
-    var monImage = document.createElement('image');
-    image.src = `#${response.id}`;
 
-    id++;
-
-  }
-  
-  window.addEventListener("load",demarrage);
-  
-  button.addEventListener("click", photos);
-  
-}
